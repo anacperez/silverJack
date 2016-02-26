43089b6528dcf1fb917992f443110bf4bb06721e
Ana Perez <anaperez@csumb.edu>
2016-02-26 01:50:09 +0000

    declared arrays and setup styles.css

--- silverJack.php
+++ silverJack.php
@@ -1,3 +1,23 @@
 <?php
 
+$suits = array("clubs", "dimonds", "hearts", "spades");
+$deck = array();
+for($i = 0; $i < 53; $i++)
+{
+    $deck[] = $i;
+}
+shuffle($deck);
+
 ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>
+            
+        </title>
+        <link rel="stylesheet" type = "text/css" href="styles.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
