80dea9f32123d37f72c3356f831d303c0b39a8d9
Ana Perez <anaperez@csumb.edu>
2016-02-27 18:51:41 +0000

    edited styles

--- silverJack.php
+++ silverJack.php
@@ -1,6 +1,6 @@
 <?php
 
-$suits = array("clubs", "dimonds", "hearts", "spades");
+$suits = array("clubs", "diamonds", "hearts", "spades");
 $deck = array();
 for($i = 0; $i < 53; $i++)
 {
@@ -13,11 +13,13 @@ shuffle($deck);
 <html>
     <head>
         <title>
-            
+            SilverJack!
         </title>
         <link rel="stylesheet" type = "text/css" href="styles.css">
     </head>
     <body>
-        
+        <div id="header1">
+            <h1><center>SilverJack</center></h1>
+        </div>
     </body>
 </html>
